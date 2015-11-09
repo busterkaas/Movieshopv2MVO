326fce25aa081731b51bf6cadf359bfbb6ece0e5
Kennie <the_reborn92@hotmail.com>
2015-11-09 10:14:00 +0100

    Virtuel slettet

--- DtoModel/GenreDTO.cs
+++ DtoModel/GenreDTO.cs
@@ -10,6 +10,6 @@ namespace DtoModel
     {
         public int GenreId { get; set; }
         public string Name { get; set; }
-        public virtual ICollection<MovieDTO> Movies { get; set; }
+        public  ICollection<MovieDTO> Movies { get; set; }
     }
 }
