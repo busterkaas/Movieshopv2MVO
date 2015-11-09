a9659db5e7708976f151e8d8188b6d643e5b86d5
DennP7791 <cookie070791@gmail.com>
2015-11-09 09:47:16 +0100

    DTO Genres

--- DtoModel/Genre.cs
+++ DtoModel/Genre.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +9,8 @@ namespace DtoModel
 {
     class Genre
     {
+        public int GenreId { get; set; }
+        public string Name { get; set; }
+        public virtual List<Movie> Movies { get; set; }
     }
 }
