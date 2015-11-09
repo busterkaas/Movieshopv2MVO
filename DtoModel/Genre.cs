dbe26c350930e11293541b098be25b6173e6e338
DennP7791 <cookie070791@gmail.com>
2015-11-09 09:53:01 +0100

    genre

--- DtoModel/Genre.cs
+++ DtoModel/Genre.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace DtoModel
 {
-    class Genre
+    public class Genre
     {
         public int GenreId { get; set; }
         public string Name { get; set; }
