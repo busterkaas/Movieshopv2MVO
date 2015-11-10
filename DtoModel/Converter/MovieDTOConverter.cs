4f673b26b4ca3a461a5663bf0458d063ee36e7fb
Tas635171 <tasin-a@hotmail.com>
2015-11-10 09:17:09 +0100

    MovieConverter

---
+++ DtoModel/Converter/MovieDTOConverter.cs
@@ -0,0 +1,37 @@
+﻿using MovieShopDAL.BE;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DtoModel.Converter
+{
+    class MovieDTOConverter : AbstractDtoConverter<Movie, MovieDTO>
+    {
+        public override MovieDTO Convert(Movie movie)
+        {
+            var dto = new MovieDTO()
+            {
+                MovieId = movie.MovieId,
+                GenreId = movie.GenreId,
+                Title = movie.Title,
+                Year = movie.Year,
+                Price = movie.Price,
+                ImageUrl = movie.ImageUrl,
+                TrailerUrl = movie.TrailerUrl
+            };
+
+            if (movie.Genre != null)
+            {
+                dto.Genre = new GenreDTO()
+                {
+                    GenreId = movie.Genre.GenreId,
+                    Name = movie.Genre.Name
+                };
+            }
+
+            return dto;
+        }
+    }
+}
